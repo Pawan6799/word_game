4196a3e54a4c8463021e2cfc6c09b04cdfb32219
Pawan6799 <pawan.kumar6799@gmail.com>
2022-02-27 12:19:29 +0530

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const named=['Birds','Cricket','Football','Hockey','Basketball','Baseball','News','Report'];
 const starts=document.getElementById("start");
 const text=document.getElementById("text1");
-//const search=document.getElementById("search1");
+const lose=document.getElementById("childlose");
 const right1=document.getElementById("correct");
 const wrong1=document.getElementById("itswrong1");
 const button=document.getElementById("button");
 const started=document.getElementById("started");
+const childs=document.getElementById("child");
+const cross=document.getElementById("cross");
 starts.innerHTML=named[Math.floor(Math.random()*named.length)];
-var x=50;
+var x=40;
 let count=0;
 let counts=0;
+cross.addEventListener("click",function(){
+    childs.style.display="none";
+    lose.style.display="none";
+
+});
 button.addEventListener("click",function start(){
 //function start(){
     started.style.display="flex";
     started.style.top=x+"px";
     x++;
-    if(x==438){
+    if(x==440){
+        if(count==10){
+            childs.style.display="flex";
+        }
         if(starts.innerText==text.value && count<11){
             //right.style.display="flex";
             let correct_text=`<div class="cricket" id="itsright">${starts.innerText}</div>`
@@ -26,20 +36,35 @@ button.addEventListener("click",function start(){
         
         else if(starts.innerText!=text.value && counts<11){
             // wrong.style.display="flex";
+            if(counts==10){
+                lose.style.display="flex";
+            }
             if(text.value!=""){
              let wrong_text=` <div class="crickets" id="itswrong">${starts.innerText}</div>`
              console.log(wrong_text);
              wrong1.innerHTML=wrong1.innerHTML+wrong_text;
              counts=counts+1;
+            //  if(counts==10){
+            //     child.style.display="flex";
+            // }
             }
              
         }
         
-        x=50;
+        x=40;
         starts.innerHTML=named[Math.floor(Math.random()*named.length)];
 
+
     }
-   
-    requestAnimationFrame(start);
+    if(count<11 && counts<11){
+        requestAnimationFrame(start);
+    }
+    //requestAnimationFrame(start);
 });
-requestAnimationFrame(start);
+//requestAnimationFrame(start);
+// function show(){
+//     if(counts==10){
+//         child.style.display="flex";
+//     }
+// }
+// //requestAnimationFrame(start);
